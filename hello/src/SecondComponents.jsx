897433c07aaae7e9aeac26927cc79bb26d560e37
Slimane Hamdaoui <Slimane-Hamdaoui@PRG.AD>
2026-02-24 14:22:01 +0200

    functional and class components

---
+++ hello/src/SecondComponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import stylat from './secondComponents.module.css'
+
+export default function SecondComponents() {
+  return (
+    <div className = {stylat.card}>
+        <p>SecondComponents</p>
+    </div>
+  )
+}
